1796379bdfcdef059d28cc13dd3d9efb55c298a1
perissology <perissology@protonmail.com>
2018-04-24 11:18:56 -0700

    fix contract export issue

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const contracts = require('./build/contracts');
-exports.LPPDac = contracts.LPPDac;
-exports.LPPDacFactory = contracts.LPPDacFactory;
+const contractInfo = require('../build/LPPDacFactory.sol'); // const LPPDacsByteCode = require('../build/LPPDacs.sol').LPPDacsByteCode;
+const generateClass = require('eth-contract-class').default;
+
+module.exports = {
+  LPPDac: generateClass(contractInfo.LPPDacsABI, contractInfo.LPPDacsByteCode),
+  LPPDacFactory: generateClass(contractInfo.LPPDacFactoryABI, contractInfo.LPPDacFactoryByteCode),
+};
+
+module.exports = LPPDacs;
